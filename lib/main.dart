61af78f64d922b46f8f20f392970bdc76861e5f6
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2020-11-08 15:40:06 +0300

    add lib

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:inno_russian/main_screen.dart';
+import 'package:inno_russian/verification.dart';
+
+import 'sign_in.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      home: MainNavigation(),
+      routes: {
+        "/mainNavigation": (context) => MainNavigation()
+      },
+    )
+  );
+}
+
+
+
