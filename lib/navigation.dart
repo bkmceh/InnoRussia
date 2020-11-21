f398cd7072326e1cbfbf5fd5ea36f2885722d0da
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2020-11-21 15:44:12 +0300

    Refactor logic of code and add parse categories

---
+++ lib/navigation.dart
@@ -0,0 +1,8 @@
+import 'MainSreens/menu.dart';
+import 'MainSreens/settings.dart';
+import 'MainSreens/vocabulary.dart';
+
+class NavigationButton {
+
+  static final tabsScreens = [Menu(), Vocabulary(), Settings()];
+}
